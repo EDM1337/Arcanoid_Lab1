68b3a4cd361f5e7167d62b0db32bce0039bd62c3
DieievDaniel <120338641+DieievDaniel@users.noreply.github.com>
2023-04-19 20:55:31 +0300

    def reset

--- main.py
+++ main.py
@@ -177,3 +177,13 @@ class game_ball():
                            self.ball_rad)
         pygame.draw.circle(screen, paddle_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
                            self.ball_rad, 3)
+
+    def reset(self, x, y):
+        self.ball_rad = 10
+        self.x = x - self.ball_rad
+        self.y = y
+        self.rect = Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
+        self.speed_x = 4
+        self.speed_y = -4
+        self.speed_max = 5
+        self.game_over = 0
