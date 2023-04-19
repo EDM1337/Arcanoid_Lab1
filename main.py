2c487cec8129c6375906d0ca46bd9c798421e05e
Arthur Stolyarchuk <clasg4clans@gmail.com>
2023-04-19 21:08:14 +0300

    last fix

--- main.py
+++ main.py
@@ -217,26 +217,27 @@ while run:
         if game_over != 0:
             live_ball = False
 
-        # print player instructions
-        if not live_ball:
-            if game_over == 0:
-                draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
-            elif game_over == 1:
-                draw_text('YOU WON!', font, text_col, 240, screen_height // 2 + 50)
-                draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
-            elif game_over == -1:
-                draw_text('YOU LOST!', font, text_col, 240, screen_height // 2 + 50)
-                draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
+    # print player instructions
+    if not live_ball:
+        if game_over == 0:
+            draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
+        elif game_over == 1:
+            draw_text('YOU WON!', font, text_col, 240, screen_height // 2 + 50)
+            draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
+        elif game_over == -1:
+            draw_text('YOU LOST!', font, text_col, 240, screen_height // 2 + 50)
+            draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.MOUSEBUTTONDOWN and live_ball == False:
-                live_ball = True
-                ball.reset(player_paddle.x + (player_paddle.width // 2), player_paddle.y - player_paddle.height)
-                player_paddle.reset()
-                wall.create_wall()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and live_ball == False:
+            live_ball = True
+            ball.reset(player_paddle.x + (player_paddle.width // 2), player_paddle.y - player_paddle.height)
+            player_paddle.reset()
+            wall.create_wall()
 
-        pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
+
