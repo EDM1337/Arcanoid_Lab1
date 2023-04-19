161f69989f99b40c93302090c0e287d9c58ca321
DieievDaniel <120338641+DieievDaniel@users.noreply.github.com>
2023-04-19 20:50:50 +0300

    next step

--- main.py
+++ main.py
@@ -90,3 +90,17 @@ class paddle():
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
             self.rect.x += self.speed
             self.direction = 1
+
+    def draw(self):
+        pygame.draw.rect(screen, paddle_col, self.rect)
+        pygame.draw.rect(screen, paddle_outline, self.rect, 3)
+
+    def reset(self):
+
+        self.height = 20
+        self.width = int(screen_width / cols)
+        self.x = int((screen_width / 2) - (self.width / 2))
+        self.y = screen_height - (self.height * 2)
+        self.speed = 10
+        self.rect = Rect(self.x, self.y, self.width, self.height)
+        self.direction = 0
