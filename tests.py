522227b2d6cf73479ec07332afbea924ada7f1ba
Arthur Stolyarchuk <clasg4clans@gmail.com>
2023-05-17 19:38:59 +0300

    some fixes #2

--- tests.py
+++ tests.py
@@ -1,12 +1,7 @@
-import pygame
-import pytest
-from pygame.locals import *
-from main import wall, paddle, game_ball, draw_text, game_over, ball, player_paddle, is_game_over
-from unittest.mock import Mock, MagicMock, patch
+from main import paddle, game_ball, is_game_over
 
 import pytest
-from pygame.locals import *
-from unittest.mock import Mock
+
 screen_width = 600
 
 
@@ -19,13 +14,16 @@ def create_player_paddle():
 # Fixture for creating a game ball object
 @pytest.fixture
 def create_game_ball(create_player_paddle):
-    return game_ball(create_player_paddle.x + (create_player_paddle.width // 2), create_player_paddle.y - create_player_paddle.height)
+    return game_ball(create_player_paddle.x +
+                     (create_player_paddle.width // 2), create_player_paddle.y - create_player_paddle.height)
 
 
 # Test case for the reset method of game_ball class
 def test_game_ball_reset(create_player_paddle, create_game_ball):
-    create_game_ball.reset(create_player_paddle.x + (create_player_paddle.width // 2), create_player_paddle.y - create_player_paddle.height)
-    assert create_game_ball.rect.x == create_player_paddle.x + (create_player_paddle.width // 2) - create_game_ball.ball_rad
+    create_game_ball.reset(create_player_paddle.x +
+                           (create_player_paddle.width // 2), create_player_paddle.y - create_player_paddle.height)
+    assert create_game_ball.rect.x == create_player_paddle.x + \
+           (create_player_paddle.width // 2) - create_game_ball.ball_rad
     assert create_game_ball.rect.y == create_player_paddle.y - create_player_paddle.height
     assert create_game_ball.speed_x == 4
     assert create_game_ball.speed_y == -4
@@ -45,7 +43,6 @@ def test_ball_bounce_off_wall():
     # Assert the expected outcome of the bounce
     assert game_ball_instance.speed_x > 0  # Ball should reverse its horizontal direction
     # Add more assertions as needed for other properties affected by the bounce
-
 
 
 def test_game_over():
